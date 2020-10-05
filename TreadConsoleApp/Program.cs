95a7d39f383409efb561f008cd4ed0ca731b994f
Danilo <didox_59@yahoo.com.br>
2020-10-05 11:23:05 -0300

    exemplo tread

---
+++ TreadConsoleApp/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading;
+
+namespace TreadConsoleApp
+{
+    class Program
+    {
+        static async void Main(string[] args)
+        {
+            Thread t = new Thread(Print);
+            t.Start();
+
+            for (int i = 0; i < 1000; i++)
+            {
+                Console.Write(0);
+            }
+        }
+
+        private static void Print()
+        {
+            for(int i=0; i < 1000; i++)
+            {
+                Console.Write(1);
+            }
+        }
+    }
+}
